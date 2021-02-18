8691e49ce8df59357124b2c4cd2cc848bddeed8f
Robert D Lundberg <robdlundberg@gmail.com>
2021-02-18 12:01:09 -0800

    Set up question format and corrected files

---
+++ index.js
@@ -0,0 +1,63 @@
+// packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+//questions to ask user 
+const questions = [
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+    {
+        type: "input",
+        message: "",
+        name: "", 
+    }
+];
+// TODO: Include packages needed for this application
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
