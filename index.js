8937a3a2d4110db8cfd7a0d0d80af1db1cbb887a
Robert D Lundberg <robdlundberg@gmail.com>
2021-02-18 17:34:46 -0800

    Spent a lot of time fixing minor syntax errors. Complete index file.

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
 // packages
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatorMarkdown = require('');
+const generatorMarkdown = require('generateMarkdown.js');
 
 
 //questions to ask user 
 const questions = [
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What is the project's name?",
+        name: "Title", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "Please describe your project.",
+        name: "Description", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What dependencies does the user need to install to run this app?",
+        name: "Installation", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What is this app used for?",
+        name: "Usage", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What command do you use to test this app?",
+        name: "Test", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What license will be used?",
+        name: "License", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "Project Contributors",
+        name: "Contributor", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What is your GitHub Username?",
+        name: "GitHub", 
+    },
     {
         type: "input",
-        message: "",
-        name: "", 
-    }
+        message: "What is your email?",
+        name: "Email", 
+    } 
 ];
 
 // function to write README file 
 function writeToFile(fileName, data) {
-
     fs.writeFile(fileName, data, function(err) {
         console.log(fileName)
         console.log(data)
@@ -63,9 +62,9 @@ function writeToFile(fileName, data) {
             return console.log(err);
         } else {
         console.log ("Success");
-        }    
+        } 
+    })
 }
-
 // function to initialize app
 function init() {
     inquirer.prompt(questions)
@@ -73,7 +72,7 @@ function init() {
             writeToFile("README.md", generatorMarkdown(data));
             console.log(data)
         })
-}
+    }
 
 // Function call to initialize app
 init();
